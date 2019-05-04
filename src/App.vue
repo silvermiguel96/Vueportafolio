437d53c63e300a8745b0f4421d710f103a22b715
Miguelangel <silvermiguel96@gmail.com>
2019-05-04 15:44:10 -0500

    Maquetando y agregando hero

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,28 @@
 <template>
   <div id="app">
-    <h1> {{ person }}</h1>
+    <div class="container-left">
+    <PFHero v-bind:person="person">
+    </PFHero>
+    </div>
+    <div class="container-right">
+
+    </div>
+    <!-- <h1> {{ person.data[0].picture }}</h1> -->
   </div>
 </template>
 
 <script>
 import axios from 'axios'
 import URL from './services/url.js'
-import { ServerResponse } from 'http';
+
+//Componenets
+import PFHero from '../src/components/hero.vue'
 
 export default {
   name: 'app',
+  components: {
+    PFHero
+  },
   data () {
     return { 
       person: [],
@@ -26,4 +38,36 @@ export default {
 </script>
 
 <style>
+*{
+  margin: 0;
+  padding: 0;
+}
+h1,
+  h2,
+    h3,
+    p,
+    li,
+    {
+      font-family:Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+}
+p {
+  font-size: 20px;
+}
+#app {
+  display: flex;
+  width: 100%;
+  height: 100vh;
+}
+.container-left{
+  background-color: #252f63;
+  color: #fff;
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+.container-right {
+  background-color: #000000;
+  width: 50%
+}
 </style>
---
+++ src/components/hero.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="hero">
+    <figure>
+      <img v-bind:src="person.data[0].picture.large" alt="Hero imagen" >
+    </figure>
+  </section>
+</template>
+<script>
+export default {
+  props : {
+    person: {
+      type: Object,
+      required: true
+    }
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+<style>
+.hero {
+  display: flex;
+  height: 400px;
+  width: 100%;
+  flex-direction: row;
+  justify-content: center;
+}
+
+.hero figure img {
+  border-radius: 50%;
+}
+</style>
